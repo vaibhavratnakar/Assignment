bb02625f1778224955a47e0fa956b1fbbf6ec353
Vaibhav ratnakar <vaibhavratnakar@github.com>
2018-03-05 01:19:34 +0530

    Revert "26"
    This reverts commit 7c08f56573690b538fbe0eb198d937630d236874.

--- hello.c
+++ hello.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
 
+void microkernel_sendmsg(char *);
+
 void main(){
 
-	printf("Helloworld!\n");
-	printf("This must be a monolithic design\n");
+printf("Helloworld!\n");
+printf("This must be a monolithic design\n");
+
+	
 	microkernel_sendmsg("is more portable");
 }
 
