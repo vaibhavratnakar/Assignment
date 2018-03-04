ff2f63b01b7eafeca721b780fe9682d5af5ff975
Vaibhav ratnakar <vaibhavratnakar@github.com>
2018-03-05 00:47:03 +0530

    21

--- hello.c
+++ hello.c
@@ -1,17 +1,9 @@
 #include<stdio.h>
 
-void microkernel_sendmsg(char *);
-
 void main(){
 
-printf("Helloworld!\n");
-printf("This must be a monolithic design\n");
-
-	
-	microkernel_sendmsg("is more portable");
+	printf("Helloworld!\n");
+	printf("This must be a monolithic design\n");
 }
 
-void microkernel_sendmsg(char *a){
-	printf("microkernel: %s\n", a);
 
-}
